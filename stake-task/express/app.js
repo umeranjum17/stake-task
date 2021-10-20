ee138b6f63810078bf1ca5bc2fdd6427bcf8d8bb
UmerFarooq-Odiggo <89737818+UmerFarooq-Odiggo@users.noreply.github.com>
2021-10-20 22:34:01 +0400

    Update app.js

--- stake-task/express/app.js
+++ stake-task/express/app.js
@@ -3,8 +3,6 @@ const express = require('express');
 const routes = {
 	investment: require('./routes/investment'),
 	property: require('./routes/property'),
-	// Add more routes here...
-	// items: require('./routes/items'),
 };
 
 const app = express();
@@ -28,7 +26,7 @@ app.get('/', (req, res) => {
 	res.send(`
 		<h2>Hello, Sequelize + Express!</h2>
 		<p>Make sure you have executed <b>npm run setup-example-db</b> once to have a populated example database. Otherwise, you will get <i>'no such table'</i> errors.</p>
-		<p>Try some routes, such as <a href='/api/users'>/api/users</a> or <a href='/api/orchestras?includeInstruments'>/api/orchestras?includeInstruments</a>!</p>
+		<p>Try some routes, such as <a href='/api/users'>/api/property</a> or <a href='/api/orchestras?includeInstruments'>/api/orchestras?includeInstruments</a>!</p>
 		<p>To experiment with POST/PUT/DELETE requests, use a tool for creating HTTP requests such as <a href='https://github.com/jakubroztocil/httpie#readme'>HTTPie</a>, <a href='https://www.postman.com/downloads/'>Postman</a>, or even <a href='https://en.wikipedia.org/wiki/CURL'>the curl command</a>, or write some JS code for it with <a href='https://github.com/sindresorhus/got#readme'>got</a>, <a href='https://github.com/sindresorhus/ky#readme'>ky</a> or <a href='https://github.com/axios/axios#readme'>axios</a>.</p>
 	`);
 });
